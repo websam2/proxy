491d480d8c7366330f7f69289085fdbab8a72431
Samuel Z <websamrgt@gmail.com>
2024-08-16 16:17:55 -0300

    Configure for Railway

--- proxy_server.py
+++ proxy_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import yt_dlp
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,4 @@ def proxy_download():
         }), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
