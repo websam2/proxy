986ef98266dee0f86370a31cef97773167b10b2a
Samuel Zezilia <104931704+websam2@users.noreply.github.com>
2024-08-19 08:45:52 -0300

    Update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import yt_dlp
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/proxy_download', methods=['POST'])
 def proxy_download():
